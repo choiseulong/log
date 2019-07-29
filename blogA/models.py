f512bd5adc35e90f5d29126be8f71c2e98999789
seulong <choiseulong@likelion.org>
2019-07-29 16:06:11 +0900

    models.py img

--- blogA/models.py
+++ blogA/models.py
@@ -5,9 +5,9 @@ class Blog(models.Model) :
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     body = models.TextField()
-
+    img = models.ImageField(blank=True)
     def __str__(self) :
         return self.title
 
-    def summary(self):
+    def summary(self) :
         return self.body[:100]
