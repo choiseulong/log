edff7d2bd9b18c4cf6cb6cc54827f613952f580b
seulong <choiseulong@likelion.org>
2019-07-29 16:17:01 +0900

    delete img models

--- blogA/models.py
+++ blogA/models.py
@@ -5,7 +5,6 @@ class Blog(models.Model) :
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     body = models.TextField()
-    img = models.ImageField(blank=True)
     def __str__(self) :
         return self.title
 
